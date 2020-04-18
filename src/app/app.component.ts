3baaabb063872c387ef7bc7152d253a20055079c
Wataru.Kasahara <kasahara1019@gmail.com>
2020-04-18 17:35:38 +0900

    各種データ更新実装

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http: HttpClient) {}
   title = 'try-zone-less-angular';
+
+  messageSubject = new BehaviorSubject<string>('');
+  user$: Observable<any | null> = of(null);
+
+  get message$() {
+    return this.messageSubject.asObservable();
+  }
+
+  ngOnInit() {
+    this.fetchUser();
+  }
+
+  // NOTE: click event
+  showMessage() {
+    this.messageSubject.next('Hello world');
+  }
+
+  // NOTE: Http request
+  fetchUser() {
+    this.user$ = this.http.get('https://jsonplaceholder.typicode.com/users/1');
+  }
 }
