03ad05100387712a6c330256c2a59a60a7bf9293
Wataru.Kasahara <kasahara1019@gmail.com>
2020-04-19 09:30:22 +0900

    ReactiveComponentModule の import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveComponentModule } from '@ngrx/component';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, ReactiveComponentModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
